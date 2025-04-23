51ef93d192cc545020a89fa6be64f0711ae2e152
Thiago Sanches <thiagomlkgame@gmail.com>
2025-04-23 14:01:24 -0300

    Documentação Atualizada

--- preload.js
+++ preload.js
@@ -17,5 +17,6 @@ contextBridge.exposeInMainWorld('api',
         newClient: (client) => ipcRenderer.send('new-client', client),
         resetForm: (args) => ipcRenderer.on('reset-form',args),
         searchName: (name) => ipcRenderer.send('search-name',name),
-        renderClient:(dataClient)=> ipcRenderer.on('render-client',dataClient)
+        renderClient:(dataClient)=> ipcRenderer.on('render-client',dataClient),
+        validateSearch: () => ipcRenderer.send('validate-search')
     })
